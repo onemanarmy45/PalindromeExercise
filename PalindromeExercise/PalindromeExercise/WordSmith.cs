2eff16e5dbf9102b645a82aea4b8ef2dc3e76cc9
rob.jordan054@gmail.com <rob.jordan054@gmail.com>
2023-04-03 09:54:54 -0400

    palindromeos

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+
+		public bool IsAPalindrome(string word)
+		{
+			if (word == null || word.Length == 0)
+			{
+				return false;
+			}
+
+			var reversed = "";
+
+			for (var i = word.Length -1; i >=0; i--)
+			{
+				reversed += word.ToLower()[i];
+			}
+
+			if (reversed == word.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}	
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("mule", false)]
+        [InlineData("huell", false)]
+        [InlineData("fuel", false)]
+        [InlineData("tacocat", true)]
+        [InlineData("racecar", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            WordSmith tester = new WordSmith();
 
+            var actual = tester.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
